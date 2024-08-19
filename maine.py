d43809804388ac04b5d19f0bb395f65c3c914cff
a.griaznov <a.griaznov@prosoftsystems.ru>
2024-08-19 15:48:50 +0500

    Ex5: Парсинг JSON

--- maine.py
+++ maine.py
@@ -1,4 +1,10 @@
-import requests
+import requests, json
 
+#Ex4: GET-запрос
 response = requests.get('https://playground.learnqa.ru/api/get_text')
 print(response.text)
+
+# Ex5: Парсинг JSON
+json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+obj = json.loads(json_text)
+print(obj['messages'][1]['message'])
