8953c58bd32fd0e8b012c381e2a32655ec4d782b
a.griaznov <a.griaznov@prosoftsystems.ru>
2024-08-19 16:33:29 +0500

    Ex7: Запросы и методы

--- maine.py
+++ maine.py
@@ -7,11 +7,58 @@ print(response.text)
 # Ex5: Парсинг JSON
 json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
 obj = json.loads(json_text)
-print(obj['messages'][1]['message'])
+print(obj['messages'][1]['message'], '\n')
 
 # Ex6: Длинный редирект
 response = requests.get('https://playground.learnqa.ru/api/long_redirect')
 
 print('Ex6: Длинный редирект')
 print("Колличество редиректов: ", len(response.history))
-print("Конечный URL: ",response.history[1].url)
+print("Конечный URL: ",response.history[1].url, '\n')
+
+
+# Ex7: Запросы и методы
+print('Ex7: Запросы и методы')
+
+url = 'https://playground.learnqa.ru/ajax/api/compare_query_type'
+
+response = requests.get(url)
+print('Ответ на пустой get: ',response.text)
+
+response = requests.post(url)
+print('Ответ на пустой post: ', response.text)
+
+
+response = requests.head(url)
+print('Статус код на head: ', response.status_code, ' - неправильный запрос')
+print('В теле ответа ничего: ', response.text, '\n')
+
+methods = ['GET', 'POST', 'PUT', 'DELETE']
+def cheker(method, req_method, response):   
+    if method == req_method and response.text == '{"success":"!"}':
+        print(method, req_method, response.text, ' Ok')
+    elif method == req_method and response.text != '{"success":"!"}':
+        print(method, req_method, response.text, ' Fail. Ожидался {"success":"!"}')
+    elif method != req_method and response.text == '{"success":"!"}':
+        print(method, req_method, response.text, ' Fail. Ожидался Wrong method provided')
+    else:
+        print(method, req_method, response.text, ' Ok')
+
+for method in methods:
+    
+    req_method = 'GET'
+    response = requests.get(url, params={'method': method})
+    cheker(method, req_method, response)
+
+    req_method = 'POST'
+    response = requests.post(url, data={'method': method})
+    cheker(method, req_method, response)
+
+    req_method = 'PUT'
+    response = requests.put(url, data={'method': method})
+    cheker(method, req_method, response)
+
+    req_method = 'DELETE'
+    response = requests.delete(url, data={'method': method})
+    cheker(method, req_method, response)
+
