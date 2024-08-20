c5a64c265e91df3bb62b195d9283df9cc9e1d89d
a.griaznov <a.griaznov@prosoftsystems.ru>
2024-08-20 09:11:42 +0500

    Ex11: Тест запроса на метод cookie

--- test_ex.py
+++ test_ex.py
@@ -1,5 +1,13 @@
-import pytest
+import pytest, requests
 
+# Ex10: Тест на короткую фразу
 def test_short():
     phrase = input("Set a phrase: ")
     assert len(phrase) < 15, 'Too long phrase'
+
+# Ex11: Тест запроса на метод cookie
+def test_check_cookies():
+    url = 'https://playground.learnqa.ru/api/homework_cookie'
+    response = requests.get(url)
+    print(dict(response.cookies))
+    assert dict(response.cookies)['HomeWork'] == 'hw_value', 'Answer is changed'
